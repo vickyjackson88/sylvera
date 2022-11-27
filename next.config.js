63a97e90e976a9fe84c4447dde1f9dba6ba3bad2
Vicky Jackson <vicky.jackson@qikserve.com>
2022-11-27 21:59:00 +0000

    Use permanent redirect for landing page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects/all',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
