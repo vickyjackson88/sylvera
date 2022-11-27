6f859142cb6e0038e5e73cd9757e05059d8e25b7
Vicky Jackson <vicky.jackson@qikserve.com>
2022-11-27 21:58:30 +0000

    Update App with loading case using new hook and loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/App.module.css';
+
+export const Loading = () => {
+  return <div className={styles.loading}>Loading</div>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import { Plus_Jakarta_Sans } from "@next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
+import { Loading } from "../components/Loading";
+import { usePageLoading } from "../hooks/loading";
+import styles from "../styles/App.module.css";
+
+const jakarta = Plus_Jakarta_Sans({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+});
+
+export const App = ({ Component, pageProps }: AppProps) => {
+  const { isPageLoading } = usePageLoading();
+
+  return (
+    <main className={jakarta.className}>
+      <div className={styles.container}>
+        {isPageLoading ? <Loading /> : <Component {...pageProps} />}
+      </div>
+    </main>
+  );
+};
+
+export default App;
