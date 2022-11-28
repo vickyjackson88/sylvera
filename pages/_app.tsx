8a24852da7754f19758910d741fa17d7bb81e2b0
Vicky Jackson <vicky.jackson@qikserve.com>
2022-11-28 00:20:49 +0000

    Add hero image with styling to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { Plus_Jakarta_Sans } from "@next/font/google";
 
+import hero from "../public/hero.png";
+
 import "../styles/globals.css";
 import { Loading } from "../components/Loading";
 import { usePageLoading } from "../hooks/loading";
 import styles from "../styles/App.module.css";
+import Image from "next/image";
 
 const jakarta = Plus_Jakarta_Sans({
   weight: ["300", "400", "500"],
@@ -18,7 +21,14 @@ export const App = ({ Component, pageProps }: AppProps) => {
   return (
     <main className={jakarta.className}>
       <div className={styles.container}>
-        {isPageLoading ? <Loading /> : <Component {...pageProps} />}
+        <Image className={styles.hero} src={hero} alt={"cool hero banner"} />
+        {isPageLoading ? (
+          <Loading />
+        ) : (
+          <div className={styles.content}>
+            <Component {...pageProps} />
+          </div>
+        )}
       </div>
     </main>
   );
